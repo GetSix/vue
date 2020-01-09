c158e66815790a7044a01566873cea0a44223534
xuzhaokuan <1099136480@qq.com>
2020-01-09 16:06:26 +0800

    jj

--- vue-shop-app/src/views/Eat.vue
+++ vue-shop-app/src/views/Eat.vue
@@ -1,5 +1,20 @@
 <template>
     <div class="eat">
         <h1>吃什么</h1>
+        <ul>
+            <li></li>
+        </ul>
     </div>
 </template>
+<script>
+export default {
+    data(){
+        return {
+            list:[]
+        }
+    },
+    methods: {
+        
+    },
+}
+</script>
