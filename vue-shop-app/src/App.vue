61df496318a33a276aa3f1df992b9ea27fcf9e32
dingxiaofei <1334720919@qq.com>
2020-01-13 17:59:07 +0800

    在主路由修改购物车数量

--- vue-shop-app/src/App.vue
+++ vue-shop-app/src/App.vue
@@ -6,11 +6,49 @@
       <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
       <van-tabbar-item to="/sort/tuijian" icon="search">分类</van-tabbar-item>
       <van-tabbar-item to="/eat" icon="star-o">吃什么</van-tabbar-item>
-      <van-tabbar-item to="/cart" icon="cart-o" info="5">购物车</van-tabbar-item>
+      <van-tabbar-item to="/cart" icon="cart-o" :info="num">购物车</van-tabbar-item>
       <van-tabbar-item :to="{name:'user'}" icon="user-o">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
+<script>
+import axios from 'axios';
+export default {
+  data(){
+    return{
+      num:0,
+    }
+  },
+  created() {
+   axios.get("http://192.168.16.29:3009/api/v1/shop_carts",{
+     headers:{
+       authorization: " Bearer " +localStorage.getItem("token")
+     }
+   })
+    .then(res =>{
+      console.log(res);
+       console.log(res.data.products.length);
+      this.num = res.data.products.length;
+      // console.log(res.data.products.length);
+    });
+  },
+  methods: {
+    goodsNum(){
+       axios.get("http://192.168.16.29:3009/api/v1/products", {
+          params: {
+            per: 5
+          }
+        })
+    .then(res =>{
+      console.log(res);
+      this.num = res.data.products.length;
+      console.log(res.data.products.length);
+      
+    });
+    }
+  },
+}
+</script>
 
 <style>
 #app {
