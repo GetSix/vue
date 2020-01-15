bbc2149acaa389a7f320b18704feeb9582327277
xuzhaokuan <1099136480@qq.com>
2020-01-15 13:43:00 +0800

    地图功能

--- vue-shop-app/src/main.js
+++ vue-shop-app/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+
+import VueI18n from 'vue-i18n'
 import Vant from 'vant';
 import 'vant/lib/index.css';
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+Vue.use(VueI18n) // 通过插件的形式挂载
 
 Vue.use(Vant);
 
